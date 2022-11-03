d799493e7dc080da46abc1cefe4fbb25e83133ce
panaoshuai <923068860@qq.com>
2022-11-03 17:45:28 +0800

    更新

--- pages/api/[tokenId].js
+++ pages/api/[tokenId].js
@@ -11,7 +11,7 @@ export default function handler(req, res) {
       "id": tokenId,
       name: "Texas Pass #" + tokenId, // Texas Pass #1
       // description: "Crypto Dev is a collection of developers in crypto", // 描述
-      // image: image_url + 1 + ".gif", // 
+      image: image_url + 1 + ".gif", // 
       animation_url:image_url + 1 + ".webp", // 
       attributes: [
         {
